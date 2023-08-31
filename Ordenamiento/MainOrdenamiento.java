e1851cde0a8e5adecba9ebaa64e45675b23a9bc5
Sofia Milagros Suppia <sofimisuppia@gmail.com>
2023-08-31 09:18:06 -0300

    .

---
+++ Ordenamiento/MainOrdenamiento.java
@@ -0,0 +1,29 @@
+package Ordenamiento;
+
+import java.util.Scanner;
+
+public class MainOrdenamiento {
+    public static void main(String[] args) {
+        
+        int op=0;
+        Scanner consola= new Scanner (System.in);
+
+        do {
+            System.out.println("---------- MENU DE OPCIONES ---------");
+            System.out.println("1. Insercion");
+            System.out.println("2. Shellsort");
+            System.out.println("3. Quicksort");
+            System.out.println("4. Salir");
+
+            switch (op) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                
+                    break;
+            }
+        } while (op!=0);
+    }
+}
